16e694d2c99ab2afa9ae658372ff4098ba6a18f4
SuperKojima <hayato.kojima@steamship.co.jp>
2025-03-05 09:43:00 +0900

    2つのpropsで分岐させてみる

---
+++ app/components/Branching/B.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  classification: 'X' | 'Y' | 'Z'
+}>()
+</script>
+
+<template>
+  <div>
+    <div>
+      B.vue
+    </div>
+    <div>
+      {{ props.classification }}
+    </div>
+  </div>
+</template>
---
+++ app/components/Branching/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import BranchingB from './B.vue'
+import BranchingC from './C.vue'
+import BranchingDefault from './Default.vue'
+
+const props = defineProps<{
+  municipality: 'A' | 'B' | 'C'
+  classification?: 'X' | 'Y' | 'Z'
+}>()
+
+function getComponent() {
+  switch (props.classification) {
+    case 'B':
+      return BranchingB
+    case 'C':
+      return BranchingC
+    default:
+      return BranchingDefault
+  }
+}
+</script>
+
+<template>
+  <component :is="getComponent()" :classification="props.classification" />
+</template>
